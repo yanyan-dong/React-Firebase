f6465f80313c15c7e2992e2d77aa348fe6c5294f
yanyan-dong <d.yy@hotmail.com>
2021-12-09 17:03:15 -0700

    init

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,14 +1,8 @@
-// if the image in src folder, need to import, then use img tag and {} when needed
-import './App.css';
-
-// useState hook: component data can be changed over time or in reaction to something
+import './App.css'
 import { useState } from 'react'
+import Title from './components/Title'
 
 function App() {
-  // const [initialState, toChangeStateValue] = useState()
-  // reevaluate the fuction with new state value
-  
-  // const [name, setName] = useState('Susan')
   const [showEvents, setShowEvents] = useState(true)
   const [events, setEvents] = useState([
     {title: "mario's birthday bash", id: 1},
@@ -19,42 +13,32 @@ function App() {
   console.log(showEvents)
 
   const handleClick = (id) => {
-    // setName('Mike')
-    // console.log(name)
-
-    // use filter() method: will not change the original array, it returns a new array which has items filtered or removed from the original one.
-    setEvents((prevEvents) => { // previous state value is guaranteed to be up to date. prevState holds is the value of state before the setState was triggered.
-      return prevEvents.filter((event) => {
-        return id !== event.id
-        // returns true will keep it, returns false will filter it
+    setEvents(prevEvents => {
+      return prevEvents.filter(event => id !== event.id)
     })
-    })
-    console.log(id)
   }
 
   return (
     <div className="App">
-      {/* <h1>My name is {name}.</h1>
-      <button onClick={handleClick}>Change name</button> */}
-       {/* conditional template: use logical & */}
-       {showEvents && (
+      {/* Title needed to inside of App div, because we can't have multiple parents inside JXS*/}
+      <Title />
+
+      {showEvents && (
         <div>
-          <button onClick={() => setShowEvents(false)}>hide events</button>
-        </div>)}
-       {!showEvents && 
-        (<div>
-        <button onClick={() => setShowEvents(true)}>show events</button>
-        </div>)}
-       {/* map() method: cycle througn an array and return a new array*/}
-       {/*use curly brace, because using dynamic JS*/}
-       {showEvents && events.map((event, index) => ( 
-         <div key={event.id}>
-           <h2>{index} - {event.title}</h2>
-           {/* not using handleClick(event.id) directly, because it will fire right away in the browser when the component is first rendered. */}
-           <button onClick={() => handleClick(event.id)}>Delete the event</button>
-         </div>
-       ))}
-       {/* return JSX, use parenteses ()*/}
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        </div>
+      )}  
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </div>
+      ))}
     </div>
   );
 }
