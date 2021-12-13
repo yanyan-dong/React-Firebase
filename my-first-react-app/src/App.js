36955db6a49f10d210a688de014f7d98334cc009
yanyan-dong <d.yy@hotmail.com>
2021-12-13 15:09:01 -0700

    Update App.js

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,8 +1,11 @@
 import './App.css'
 import { useState } from 'react'
 import Title from './components/Title'
+import Modal from './components/Modal'
+import EventList from './components/EventList'
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
   const [showEvents, setShowEvents] = useState(true)
   const [events, setEvents] = useState([
     {title: "mario's birthday bash", id: 1},
@@ -10,7 +13,7 @@ function App() {
     {title: "race on moo moo farm", id: 3}
   ])
 
-  console.log(showEvents)
+  console.log(showModal)
 
   const handleClick = (id) => {
     setEvents(prevEvents => {
@@ -18,10 +21,20 @@ function App() {
     })
   }
 
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
+
+  const abc="All the latest events in Marioland"
+
   return (
     <div className="App">
+
       {/* Title needed to inside of App div, because we can't have multiple parents inside JXS*/}
-      <Title />
+      <Title title="Events in Your Area" subtitle={abc} />
+      {/* <Title title="another title" subtitle="another subtitle" /> */}
+      
 
       {showEvents && (
         <div>
@@ -33,12 +46,35 @@ function App() {
           <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
       )}  
-      {showEvents && events.map((event, index) => (
-        <div key={event.id}>
+      {/* {showEvents && events.map((event, index) => (
+        <React.Fragment key={event.id}>
           <h2>{index} - {event.title}</h2>
           <button onClick={() => handleClick(event.id)}>delete event</button>
+        </React.Fragment>
+        // cannon use props in short <>...</> react fragment
+      ))} */}
+
+      {/* // CHALLENGE - Reusable Event List Component */}
+      {showEvents && <EventList events={events} handleClick={handleClick}/>}
+
+      {/* <Modal>
+        <h2>10% OFF COUPON CODE!</h2>
+        <p>Use the code WELCOME at the checkout.</p>
+      </Modal> */}
+
+      {!showModal && (
+        <div>
+          <h2>CHALLENGE - Showing the Modal</h2>
+          <button onClick={() => setShowModal(true)}>show modal</button>
         </div>
-      ))}
+      )}
+
+      {/* pass the handleClose function as a prop */}
+      {showModal && <Modal handleClose={handleClose}>
+        <h2>Terms and Conditions</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum totam omnis vel similique delectus temporibus ut quas praesentium quia incidunt, fuga quo maiores ipsum, eaque eveniet. Neque dolor iste ad.</p>
+      </Modal>}
+
     </div>
   );
 }
