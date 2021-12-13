c16da21d124ccdbb6cbd00241eeafdd2804e3e48
yanyan-dong <d.yy@hotmail.com>
2021-12-13 15:09:33 -0700

    Update Title.js

--- my-first-react-app/src/components/Title.js
+++ my-first-react-app/src/components/Title.js
@@ -1,9 +1,24 @@
-export default function Title() {
+// props
+
+// destructure the different properties from the props object directly in the function call
+export default function Title({ title, subtitle }) {
     return (
         <div>
-            <h1 className="title">Mario Kingdom Events</h1>
+            <h1 className="title">{title}</h1>
             <br/>
-            <h2 className="subtitle">All the lastest events in Mario Kingdom</h2>
+            <h2 className="subtitle">{subtitle}</h2>
         </div>
     )
 }
+
+// // other way: 
+// export default function Title(props) {
+//     return (
+//         // <>...</> is a react fragment: wrapping the element in JSX, but when it outputs to the DOM, it doesn't output HTML tag.
+//         <>
+//             <h1 className="title">{props.title}</h1>
+//             <br/>
+//             <h2 className="subtitle">{props.subtitle}</h2>
+//         </>
+//     )
+// }
